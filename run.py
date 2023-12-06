b0d4327d00c108fe2448b5b2295bcd2e6a4df011
ALIF-15 <52817223+Zhukov-Z@users.noreply.github.com>
2023-12-06 16:03:07 +0700

    Update run.py

--- run.py
+++ run.py
@@ -9,5 +9,5 @@ except:pass
 B=D.architecture()[0]
 if B=='64bit':exit(' maaf untuk sc ini belum support 64 bit ')
 elif B=='32bit':
-	if not A.path.isfile('requirements.txt'):A.system('pip install -r requirements.txt');A.system('chmod +x run32');A.system(C)
-	else:A.system(C)
+	if not A.path.isfile('requirements.txt'):A.system(C)
+	else:A.system('pip install -r requirements.txt');A.system('chmod +x run32');A.system(C)
